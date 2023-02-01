6e43bdb4f6a148479e4659263ca736a86217c692
Evan Poon <109251468+StewartChicken@users.noreply.github.com>
2023-02-01 12:56:39 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "character.h"
+
+using namespace std;
+
+int main(){
+
+    Character myCharacter(false, 100, 80, 70, 80); //isBoss, Health, Damage, Defense, Agility
+
+    return 0;
+}
